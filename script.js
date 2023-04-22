aa4acaa06242e184cd32dc0ca27447d435c10c34
star-cemetery <ricepaperlantern@ua.fm>
2023-04-21 19:39:00 -0700

    adding video and effects

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+alert("Hello");
+let clip = document.querySelector(".vid");
+clip.addEventListener("mouseover", function (e) {
+  clip.play();
+});
+clip.addEventListener("mouseout", function (e) {
+  clip.pause();
+});
 type =
   "text/javascript" >
   $(document).ready(function () {
